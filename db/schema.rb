df3808e53221677984664aa58197f9ca242d314d
Marcelo Escobar <escobar@br.systems>
2019-06-03 17:36:17 -0300

    added fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_06_03_194306) do
-
+ActiveRecord::Schema.define(version: 2019_06_03_203232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "activities", force: :cascade do |t|
     t.string "title"
@@ -30,12 +27,12 @@ ActiveRecord::Schema.define(version: 2019_06_03_194306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "interests", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,6 +43,9 @@ ActiveRecord::Schema.define(version: 2019_06_03_194306) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
