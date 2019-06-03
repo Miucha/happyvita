862fb08bb11c242605494519a7a6b245fd5afcbc
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-06-03 19:18:59 -0300

    routes booking create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'activities#index'
 
-  resources :activities, except: :destroy
+  resources :bookings, only: [:update, :destroy]
+
+  resources :activities, except: :destroy do
+    resources :bookings, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :addresses, only: [ :create ]
 
