901ea631ce7f83ad8bb00e979f8ae1fbb21fee27
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-06-06 14:27:32 -0300

    formulário de criaçao de address ok

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create, :new]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'users/:user_id/addresses', to: 'addresses#create', as: 'create_address'
+
+  resources :addresses, only: [:new, :create]
 
 end
