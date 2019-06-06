eeaa1a85e579cac30be7cb291f3ab93f434f0a0a
Marcelo Escobar <escobar@br.systems>
2019-06-06 19:59:19 -0300

    doing challenge validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,17 +8,24 @@ class Activity < ApplicationRecord
   has_many :activity_interests
   has_many :interests, through: :activity_interests
 
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :description, presence: true
   validates :event_date, presence: true
   validates :capacity, presence: true
   validates :address_id, presence: true
 
+  validate :event_title
   validate :date_cannot_be_in_the_past
 
   def date_cannot_be_in_the_past
     if event_date.present? && event_date < Date.today
       errors.add(:event_date, "não pode estar no passado")
+    end
+  end
+
+  def event_title
+    if event.present?
+      errors.add(:event_title, "Nome do evento não pode ficar em branco")
     end
   end
 end
