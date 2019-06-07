20aeaaa9ccf346add59ff6ec9a0013d75391ab9d
joaoapel <joaoapel@gmail.com>
2019-06-07 13:01:45 -0300

    Add geocoder to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :user
   has_many :activities
+
+  def full_address
+    [street, number, city].compact.join(‘, ‘)
+  end
+
+  geocoded_by :full_address
+
 end
