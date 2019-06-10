31c4fbd4430ac80f4d20f6da39738c0886754f43
Marcelo Escobar <escobar@br.systems>
2019-06-10 16:54:28 -0300

    user profile implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,23 @@ Rails.application.routes.draw do
   root to: 'activities#index'
 
   resources :bookings, only: [:edit, :update, :destroy]
-  get 'dashboard', to: 'pages#dashboard', as: :dashboard
-
+  get 'dashboard', to: 'pages#dashboard', as: 'dashboard'
+  
   patch 'activities/:id/cancel', to: 'activities#cancel', as: 'cancel_activity'
-
+  
   resources :activities, except: [:destroy, :new, :edit] do
     resources :bookings, only: [:create, :new]
   end
   get 'activities/:id/edit_event', to: 'activities#edit_event', as: 'edit_event'
   get 'activities/:id/edit_challenge', to: 'activities#edit_challenge', as: 'edit_challenge'
-
+  
   get 'new_event', to: 'activities#new_event'
   get 'new_challenge', to: 'activities#new_challenge'
-
+  
   get 'users_localization', to: 'users#index'
+  get 'users', to: 'users#profile', as: 'users_profile'
+  get 'users/:id/edit_profile', to: 'users#edit_profile', as: 'edit_users_profile'
+  patch 'users/:id/update_profile', to: 'users#update_profile', as: 'update_users_profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :addresses, only: :create
