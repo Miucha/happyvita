b4b40d5f3178a92cb30061d3789975c616e738b4
Miucha Salge <miucha.salge@gmail.com>
2019-06-10 15:25:27 -0300

    Add review photo, rate and comment to Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_150247) do
+ActiveRecord::Schema.define(version: 2019_06_10_182200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 2019_06_07_150247) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "review_photo"
+    t.integer "review_rate"
+    t.text "review_comment"
     t.index ["activity_id"], name: "index_bookings_on_activity_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
