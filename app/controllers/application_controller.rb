75ac042bf2182d30ed642376442b8bfaeebfba92
Marcelo Escobar <escobar@br.systems>
2019-06-11 16:25:53 -0300

    Adjustments in many files to go online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index, :show]
+  
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
