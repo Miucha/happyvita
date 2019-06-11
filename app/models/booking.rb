a555dddeacefa9feb30ff08e58c1544d23a633f6
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-06-11 17:30:26 -0300

    booking date validate ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,11 +4,13 @@ class Booking < ApplicationRecord
   belongs_to :activity
   belongs_to :user
 
+  validates :schedule_date, presence: true
+
   validate :date_cannot_be_in_the_past
 
   def date_cannot_be_in_the_past
-    if schedule_date.present? && event_date < Date.today
-      errors.add(:event_date, "não pode estar no passado")
+    if schedule_date.present? && schedule_date < Date.today
+      errors.add(:schedule_date, "passado")
     end
   end
 end
